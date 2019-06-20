8716bfdc5258c756ce7bf75979323e3d908cde0d
Xiaoqi Ma <xiaoqima2013@gmail.com>
2019-06-20 16:04:20 +0200

    update logging func

--- shapSD/log_execution_time.py
+++ shapSD/log_execution_time.py
@@ -1,43 +1,41 @@
-import os
 import time
 import functools
 import logging
 
 
 def init_logging(logfile):
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(module)s: %(message)s',
-                                  datefmt='%m/%d/%Y %H:%M:%S')
-    fh = logging.FileHandler(logfile)
-    fh.setFormatter(formatter)
-    logging.getLogger().addHandler(fh)
-    logging.getLogger().setLevel(logging.INFO)
-
-
-def get_logger(logfile):
     logger = logging.getLogger(logfile)
+    if not len(logging.getLogger(logfile).handlers):
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(module)s: %(message)s',
+                                      datefmt='%m/%d/%Y %H:%M:%S')
+        fh = logging.FileHandler(logfile)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+        logger.setLevel(logging.INFO)
     return logger
+
+# def get_logger(logfile):
+#     logger = logging.getLogger(logfile)
+#     return logger
 
 
 def func_logging(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        if len(args) > 0 and isinstance(args[0], str):
-            logfile = args[0]
+        if 'logfile' in kwargs.keys():
+            logfile = kwargs['logfile']
+            logfile = '../logs/{}'.format(logfile)
         else:
-            logfile = 'execution_time'
-        if os.path.exists(logfile):
-            pass
-            # print('logging file exists')
-        else:
-            init_logging(logfile)
-        logger = get_logger(logfile)
+            logfile = '../logs/execution_time.log'
 
+        logger = init_logging(logfile)
+        # print('handlers: ', logger.handlers)
         start = time.perf_counter()
         res = func(*args, **kwargs)
         end = time.perf_counter()
         msg = '{} running time: {} ms'.format(func.__name__, (end - start) * 1000)
-        print(logger.__class__)
         logger.info(msg)
+        print('logging to file: ', logfile)
         return res
 
     return wrapper
