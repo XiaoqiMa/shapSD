407248824613c6d3f21a8699ee617d4ee5ddff00
Xiaoqi Ma <xiaoqima2013@gmail.com>
2019-06-21 16:40:08 +0200

    clone from pysubgroup repository and change format to PEP8

---
+++ pysubgroup/pysubgroup/__init__.py
@@ -0,0 +1,10 @@
+from pysubgroup.subgroup import *
+from pysubgroup.algorithms import *
+from pysubgroup.measures import *
+from pysubgroup.utils import *
+
+from pysubgroup.boolean_target import *
+from pysubgroup.numeric_target import *
+from pysubgroup.fi_target import *
+
+from pysubgroup.visualization import *
