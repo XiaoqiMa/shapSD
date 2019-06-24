66a3195aaaa252b30b9c692f52a6bafb4a86e2dd
Xiaoqi Ma <xiaoqima2013@gmail.com>
2019-06-24 21:58:28 +0200

    refactor binary flip file and numeric perturb file

--- shapSD/binary_flip.py
+++ shapSD/binary_flip.py
@@ -1,23 +1,46 @@
+"""
+Try to inspect variable influence, starting with flipping binary variable
+author: Xiaoqi
+date: 2019.06.24
+"""
+import shap
 import numpy as np
 
 
-def get_prediction(X_train, model):
-    try:
-        predictions = model.predict_proba(X_train)  # classification task
-        return predictions[:, 0]
-    except AttributeError:
-        predictions = model.predict(X_train)  # regression task
-        return predictions
-    except Exception:
-        raise Exception('Model does not support probability prediction')
+class BinaryFlip(object):
 
+    def __init__(self, x_train, y_train, model, flip_attr):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.model = model
+        self.flip_attr = flip_attr
 
-def calc_flip_effect(X_train, flip_attr, model):
-    ori_prediction = get_prediction(X_train, model)
-    X_flip = X_train.copy()
-    X_flip[flip_attr] = X_flip[flip_attr].apply(lambda x: x ^ 1)
-    new_prediction = get_prediction(X_flip, model)
+    def get_prediction(self, x_train):
+        try:
+            predictions = self.model.predict_proba(x_train)  # classification task
+            return predictions[:, 0]
+        except AttributeError:
+            predictions = self.model.predict(x_train)  # regression task
+            return predictions
+        except Exception:
+            raise Exception('Model does not support probability prediction')
 
-    avg_effect = np.mean(np.abs(new_prediction - ori_prediction))
-    X_flip['effect'] = np.abs(new_prediction - ori_prediction) - avg_effect
-    return X_flip
+    def calc_flip_effect(self):
+        ori_prediction = self.get_prediction(self.x_train)
+        x_flip = self.x_train.copy()
+        x_flip[self.flip_attr] = x_flip[self.flip_attr].apply(lambda x: x ^ 1)
+        new_prediction = self.get_prediction(x_flip)
+
+        avg_effect = np.mean(np.abs(new_prediction - ori_prediction))
+        attr_name = '{}_effect'.format(self.flip_attr)
+        x_flip[attr_name] = np.abs(new_prediction - ori_prediction) - avg_effect
+        return x_flip
+
+    def calc_flip_shap_values(self):
+        explainer = shap.TreeExplainer(self.model)
+        shap_values = explainer.shap_values(self.x_train)
+        x = self.x_train.copy()
+        attr_index = list(x.columns).index(self.flip_attr)
+        attr_name = '{}_shap_values'.format(self.flip_attr)
+        x[attr_name] = shap_values[:, attr_index]
+        return x
--- shapSD/numeric_perturb.py
+++ shapSD/numeric_perturb.py
@@ -1,24 +1,48 @@
+"""
+Make perturbation on numeric variable, observe prediction changes
+author: Xiaoqi
+date: 2019.06.24
+"""
+import shap
 import numpy as np
 
 
-def get_prediction(X_train, model):
-    try:
-        predictions = model.predict_proba(X_train)  # classification task
-        return predictions[:, 0]
-    except AttributeError:
-        predictions = model.predict(X_train)  # regression task
-        return predictions
-    except Exception:
-        raise Exception('Model does not support probability prediction')
+class NumericPerturb(object):
 
+    def __init__(self, x_train, y_train, model, perturb_attr):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.model = model
+        self.perturb_attr = perturb_attr
 
-def calc_perturb_effect(X_train, perturb_attr, model):
-    ori_prediction = get_prediction(X_train, model)
-    X_perturb = X_train.copy()
-    X_perturb[perturb_attr] = np.random.permutation(X_perturb[perturb_attr].values)
-    new_prediction = get_prediction(X_perturb, model)
+    def get_prediction(self, x_train):
+        try:
+            predictions = self.model.predict_proba(x_train)  # classification task
+            return predictions[:, 0]
+        except AttributeError:
+            predictions = self.model.predict(x_train)  # regression task
+            return predictions
+        except Exception:
+            raise Exception('Model does not support probability prediction')
 
-    X_perturb['attr_change'] = np.abs(X_perturb[perturb_attr] - X_train[perturb_attr])
-    X_perturb['pred_change'] = np.abs(new_prediction - ori_prediction)
+    def calc_perturb_effect(self):
+        ori_prediction = self.get_prediction(self.x_train)
+        x_perturb = self.x_train.copy()
+        x_perturb[self.perturb_attr] = np.random.permutation(x_perturb[self.perturb_attr].values)
+        new_prediction = self.get_prediction(x_perturb)
 
-    return X_perturb
+        attr_name = '{}_change'.format(self.perturb_attr)
+        pred_attr_name = '{}_prediction_change'.format(self.perturb_attr)
+        x_perturb[attr_name] = np.abs(x_perturb[self.perturb_attr] - self.x_train[self.perturb_attr])
+        x_perturb[pred_attr_name] = np.abs(new_prediction - ori_prediction)
+        return x_perturb
+
+    def calc_perturb_shap_values(self):
+        explainer = shap.TreeExplainer(self.model)
+        shap_values = explainer.shap_values(self.x_train)
+        x = self.x_train.copy()
+        attr_index = list(x.columns).index(self.perturb_attr)
+        attr_name = '{}_shap_values'.format(self.perturb_attr)
+        x[attr_name] = shap_values[:, attr_index]
+        return x
+
